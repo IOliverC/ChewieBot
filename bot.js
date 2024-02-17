ae8979a07b42dd27f7e0a9d2e0ad1228e3a5175b
IOliverC <irene.oliver.correa4@gmail.com>
2024-02-17 13:29:23 +0100

    cambio de tematica por problemas con la key

--- bot.js
+++ bot.js
@@ -1,40 +1,21 @@
+//Librerías necesarias
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
-const token = '6608384684:AAHPKeB3QTJMJ7bVfLwaHLxhKZs5Ku6FVUEI';
+
+const token = '6519624868:AAG706F1__E1TKIUl7JEYztC8WOBki-YyMQ';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, '¡Hola! Soy NookBot, tu asistente de Animal Crossing.');
-});
+//Mensaje que se activa al iniciar
 
-//Empieza la conexion con API
-
-const axios = require('axios');
-
-const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
-
-bot.onText(/\/bug (.+)/, async (msg, match) => {
+bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-    const bugName = match[1]; // Extrae el nombre del bicho del mensaje del usuario
-
-    try {
-    const response = await axios.get(`https://acnhapi.com/v1/bugs/${bugName}`);
-    const bugData = response.data;
-
-    if (bugData.success) {
-        const bugInfo = bugData.bug;
-        const reply = `Nombre: ${bugInfo.name['name-USen']}
-                    Precio: ${bugInfo.price}
-                    Lugar: ${bugInfo.availability.location}
-                    Horario: ${bugInfo.availability.time}`;
-
-        bot.sendMessage(chatId, reply);
-    } else {
-        bot.sendMessage(chatId, 'Lo siento, no pude encontrar ese bicho.');
-    }
-    } catch (error) {
-    console.error(error);
-    bot.sendMessage(chatId, 'Lo siento, hubo un error al buscar ese bicho.');
-    }
+    bot.sendMessage(chatId, '¡Hola! Soy Chewie, tu bot de Star Wars. ¿Cómo puedo ayudarte hoy?');
 });
+
+
+//Solicitud HTTP a la API
+function buscarEnSwapi(busqueda) {
+    return axios.get(`https://www.swapi.tech/api/${busqueda}`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+}
